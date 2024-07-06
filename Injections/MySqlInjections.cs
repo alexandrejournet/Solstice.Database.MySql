14ceda61d2fe6a580a6564fafcd7d8dd10ca6340
Alexandre JOURNET <alexandre.journet@gmail.com>
2024-07-06 21:51:32 +0200

    Initial

---
+++ Injections/MySqlInjections.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Radiant.NET.Database.MySql.Injections
+{
+    public static class MySqlInjections
+    {
+
+        public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, bool migrate = false)
+        where TDbContext : DbContext
+        {
+            services.AddDbContext<TDbContext>(options =>
+            {
+#if DEBUG
+                options.EnableSensitiveDataLogging(true);
+#endif
+                options.UseMySql(config, ServerVersion.AutoDetect(config));
+            }, ServiceLifetime.Transient);
+
+            if (migrate)
+            {
+                services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
+            }
+        }
+
+        public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, ILoggerFactory? loggerFactory, bool migrate = false)
+        where TDbContext : DbContext
+        {
+            services.AddDbContext<TDbContext>(options =>
+            {
+#if DEBUG
+                options.EnableSensitiveDataLogging(true);
+                if (loggerFactory is not null)
+                {
+                    options.UseLoggerFactory(loggerFactory);
+                }
+#endif
+                options.UseMySql(config, ServerVersion.AutoDetect(config));
+            }, ServiceLifetime.Transient);
+
+            if (migrate)
+            {
+                services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Radiant.NET.Database.MySQL
+
+Helper pour MySQL
