6a83ffa3387d9c25c6146ff10ffa1e3343ef1157
Alexandre JOURNET <alexandre.journet@gmail.com>
2024-10-21 22:44:51 +0200

    wip

--- Injections/MySqlInjections.cs
+++ Injections/MySqlInjections.cs
@@ -2,47 +2,46 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Solstice.Database.MySql.Injections
+namespace Solstice.Database.MySql.Injections;
+
+public static class MySqlInjections
 {
-    public static class MySqlInjections
+
+    public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, bool migrate = false)
+        where TDbContext : DbContext
     {
-
-        public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, bool migrate = false)
-        where TDbContext : DbContext
+        services.AddDbContext<TDbContext>(options =>
         {
-            services.AddDbContext<TDbContext>(options =>
-            {
 #if DEBUG
-                options.EnableSensitiveDataLogging(true);
+            options.EnableSensitiveDataLogging(true);
 #endif
-                options.UseMySql(config, ServerVersion.AutoDetect(config));
-            }, ServiceLifetime.Transient);
+            options.UseMySql(config, ServerVersion.AutoDetect(config));
+        }, ServiceLifetime.Transient);
 
-            if (migrate)
-            {
-                services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
-            }
+        if (migrate)
+        {
+            services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
         }
+    }
 
-        public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, ILoggerFactory? loggerFactory, bool migrate = false)
+    public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, ILoggerFactory? loggerFactory, bool migrate = false)
         where TDbContext : DbContext
+    {
+        services.AddDbContext<TDbContext>(options =>
         {
-            services.AddDbContext<TDbContext>(options =>
-            {
 #if DEBUG
-                options.EnableSensitiveDataLogging(true);
-                if (loggerFactory is not null)
-                {
-                    options.UseLoggerFactory(loggerFactory);
-                }
-#endif
-                options.UseMySql(config, ServerVersion.AutoDetect(config));
-            }, ServiceLifetime.Transient);
-
-            if (migrate)
+            options.EnableSensitiveDataLogging(true);
+            if (loggerFactory is not null)
             {
-                services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
+                options.UseLoggerFactory(loggerFactory);
             }
+#endif
+            options.UseMySql(config, ServerVersion.AutoDetect(config));
+        }, ServiceLifetime.Transient);
+
+        if (migrate)
+        {
+            services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
         }
     }
 }
